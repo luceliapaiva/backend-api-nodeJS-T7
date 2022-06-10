85f46f4dd2b4f3bfd9e6165f24894bd972ff62b0
luceliapaiva <paivaluce@gmail.com>
2022-06-10 19:36:50 -0300

    express config server

--- config/database.js
+++ config/database.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose')
-const args = require("args-parser")(process.argv)
-mongoose.Promise = require("bluebird")
+const args = require('args-parser')(process.argv)
+mongoose.Promise = require('bluebird')
 
 if (args.production)
-    module.exports = mongoose.connect('mongodb://nome_banco:senha@servidor.com.br:27017/usuario' )
+    module.exports = mongoose.connect('mongodb://nome_banco:senha@servidor.com.br:27017/usuario')
 else
-    module.exports = mongoose.connect('mongodb://localhost/banco_dados')    
+    module.exports = mongoose.connect('mongodb://localhost/coders')    
 
 mongoose.Error.messages.general.required = "O campo '{PATH}' é obrigatório."
 mongoose.Error.messages.Number.min = "O '{PATH}' informado é menor que o limite mínimo de '{MIN}'."
--- config/routes.js
+++ config/routes.js
@@ -1,16 +1,16 @@
 const express = require('express')
 
-module.exports = function (server) {
+module.exports = function(server) {
     const protectedApi = express.Router();
-    server.use('/api', protectedApi);
+    server.use("/api", protectedApi);
 
-    server.use('/status', (req, res) =>
+    server.use("/status", (req, res) =>
         res.send(`BACKEND is runner.`)
     );
 
     const register = require('../api/register/registerService');
-    register.register(protectedApi, '/register');
+    register.register(protectedApi,'/register');
 
-    server.use(express.static(require('path').join(__dirname, '../public')));
+    server.use(express.static(require("path").join(__dirname, "../public")));
 }
 
